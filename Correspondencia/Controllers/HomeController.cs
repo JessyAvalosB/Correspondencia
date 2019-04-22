51f6efb4a17417e69f95ee13030a9c6f111bb2c0
Alejandro Garcia <isc_garcia@outlook.com>
2019-04-22 10:04:58 -0500

    Se agrego la vista de AddRemitente

--- Correspondencia/Controllers/HomeController.cs
+++ Correspondencia/Controllers/HomeController.cs
@@ -34,6 +34,10 @@ namespace Correspondencia.Controllers
         {
             return View();
         }
+        public IActionResult AddRemitente()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
