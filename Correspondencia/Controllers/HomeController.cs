9ac693902d2a10654609d7f0c0f2566cb3b07365
Jessy Avalos Barrales <31834551+jessyavalos1615@users.noreply.github.com>
2019-04-21 19:16:27 -0500

    fead: search view finish and addDoc create (#3)

--- Correspondencia/Controllers/HomeController.cs
+++ Correspondencia/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace Correspondencia.Controllers
             return View();
         }
 
+        public IActionResult AddDoc()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
